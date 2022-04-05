3e728d1452abd85a3f96f6d16c7228435f8bca14
yussuf ajao <ajaoyussufayo@gmail.com>
2022-04-05 17:09:27 +0100

    made some changes to the next-auth file

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -15,9 +15,9 @@ export default NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  // pages: {
-  //   signIn: "/auth/signin",
-  // },
+  pages: {
+    signIn: "/auth/signin",
+  },
   callbacks: {
     async jwt({ token, account }) {
       // Persist the OAuth access_token to the token right after signin
