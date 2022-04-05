049d1f4645beeb72ff1ed9d19479a10d0272f9a1
yussuf ajao <ajaoyussufayo@gmail.com>
2022-04-05 17:01:43 +0100

    replaced the url with /

--- pages/api/auth/signin.js
+++ pages/api/auth/signin.js
@@ -5,30 +5,19 @@ import Image from "next/image";
 const Login = ({providers}) => {
   return (
     <div className="grid place-items-center">
-      <Image
-        src="/facebook.png"
-        alt="facebook"
-        width={400}
-        height={400}
-        objectFit="contain"
-      />
+      <Image src="/facebook.png"  alt="facebook" width={400} height={400} objectFit="contain" />
       <h1 onClick={signIn}>
         {Object.values(providers).map((provider) => (
           <button
             key={provider.name}
             className="p-5 bg-blue-500 rounded-full text-white text-center cursor-pointer"
-            onClick={() =>
-              signIn(provider.id, {
-                callbackUrl: "https://facebook-clone-2-pi.vercel.app/",
-              })
-            }
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
           >
             Login with {provider.name}
           </button>
         ))}
-        {/* Login with Facebook */}
       </h1>
-    </div>
+      </div>
   );
 }
 
