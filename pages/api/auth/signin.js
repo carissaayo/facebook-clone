3e3e3a483720e544cb515bf4bd36672e9092d337
yussuf ajao <ajaoyussufayo@gmail.com>
2022-04-05 16:57:34 +0100

    removed the signout from the signin page

--- pages/api/auth/signin.js
+++ pages/api/auth/signin.js
@@ -5,20 +5,29 @@ import Image from "next/image";
 const Login = ({providers}) => {
   return (
     <div className="grid place-items-center">
-      <Image src="/facebook.png"  alt="facebook" width={400} height={400} objectFit="contain" />
+      <Image
+        src="/facebook.png"
+        alt="facebook"
+        width={400}
+        height={400}
+        objectFit="contain"
+      />
       <h1 onClick={signIn}>
         {Object.values(providers).map((provider) => (
           <button
             key={provider.name}
             className="p-5 bg-blue-500 rounded-full text-white text-center cursor-pointer"
-            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            onClick={() =>
+              signIn(provider.id, {
+                callbackUrl: "/https://facebook-clone-2-pi.vercel.app/",
+              })
+            }
           >
             Login with {provider.name}
           </button>
         ))}
         {/* Login with Facebook */}
       </h1>
-      <h2 onClick={() => signOut()}> signout</h2>
     </div>
   );
 }
